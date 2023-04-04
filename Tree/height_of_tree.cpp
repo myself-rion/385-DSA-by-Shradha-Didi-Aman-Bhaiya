1bea100b33b34fdd660188653a18ff0fdd285b34
Rion Talukder <riontalukder98@gmail.com>
2023-04-04 15:52:43 +0530

    updated height of a tree

--- Tree/height_of_tree.cpp
+++ Tree/height_of_tree.cpp
@@ -3,14 +3,15 @@ class Solution
 public:
     int height(struct Node *node)
     {
+        //base condition
         if (node == NULL)
         {
             return 0;
         }
-        int leftHeight = height(node->left);
-        int rightHeight = height(node->right);
+        int leftHeight = height(node->left); //height of left subtree
+        int rightHeight = height(node->right); // height of right subtree
 
-        int ans = max(leftHeight, rightHeight) + 1;
+        int ans = max(leftHeight, rightHeight) + 1; // max height + parent node
 
         return ans;
     }
