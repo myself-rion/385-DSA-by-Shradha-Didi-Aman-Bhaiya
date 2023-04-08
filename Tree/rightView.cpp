57fd37ac7688ee6a380ce8c0e6ac8449f30a9d96
Rion Talukder <riontalukder98@gmail.com>
2023-04-09 02:20:34 +0530

    sideways view in Tree

---
+++ Tree/rightView.cpp
@@ -0,0 +1,17 @@
+void solve(vector<int> &a, Node *root, int lvl)
+{
+    if (root == NULL)
+        return;
+
+    if (lvl == a.size()) //stepped on new level
+        a.push_back(root->data);
+
+    solve(a, root->right, lvl + 1);
+    solve(a, root->left, lvl + 1);
+}
+vector<int> rightView(Node *root)
+{
+    vector<int> ans;
+    solve(ans, root, 0);
+    return ans;
+}
