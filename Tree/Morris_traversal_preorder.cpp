1fed8232fa6ec72fb895db5592c74e925212ef95
Rion Talukder <riontalukder98@gmail.com>
2023-04-21 17:34:49 +0530

    Morris Traversal for preOrder

---
+++ Tree/Morris_traversal_preorder.cpp
@@ -0,0 +1,39 @@
+vector<int> inOrder(Node *root)
+{
+    vector<int> ans; // storing the inorder traversal
+
+    Node *curr = root;
+    while (curr != NULL)
+    {
+        if (curr->left == NULL)
+        {
+            ans.push_back(curr->data);
+            curr = curr->right;
+        }
+        else
+        {
+            Node *prev = curr->left;
+            while (prev->right != NULL && prev->right != curr)
+            {
+                prev = prev->right;
+            }
+
+            if (prev->right == NULL)
+            {
+                prev->right = curr;
+                ans.push_back(curr->data);
+                curr = curr->left;
+            }
+
+            else
+            {
+                prev->right = NULL;
+                curr = curr->right;
+            }
+        }
+    }
+    return ans;
+}
+
+Time Complexity : O(n)
+Space Complexity : O(1)
