f878f9fb874e7b1cf76d8b147444dd76ffe7bd4e
Rion Talukder <riontalukder98@gmail.com>
2023-09-13 18:59:15 +0530

    Majority Elememts 2 types

--- Array/majority_element.cpp
+++ Array/majority_element.cpp
@@ -22,3 +22,35 @@ public:
     }
 };
 // in O(1) space
+
+//majority element in an array A of size N is an element that appears more than N/2 times in the array.
+
+class Solution{
+  public:
+     
+    int majorityElement(int arr[], int size)
+    {
+        if(size==2 && arr[0]!=arr[1]) return -1;
+        
+        int res=0, count=1;
+        for(int i=1; i<size; i++){
+            if(arr[res]==arr[i]) count++;
+            else count--;
+            
+            if(count==0){
+                res=i;
+                count=1;
+            }
+        }
+        
+        count=0;
+        
+        for(int i=0; i<size; i++){
+            if(arr[i]==arr[res]) count++;
+        }
+        
+        if(count>size/2) return arr[res];
+        else return -1;
+        
+    }
+};
