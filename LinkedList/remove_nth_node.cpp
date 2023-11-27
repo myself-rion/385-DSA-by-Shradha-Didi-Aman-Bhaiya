f55fb33e537082eb7f89b55ef1f1b004a9c73c9d
Rion Talukder <riontalukder98@gmail.com>
2023-11-27 15:23:32 +0530

    remove nth node from list

---
+++ LinkedList/remove_nth_node.cpp
@@ -0,0 +1,28 @@
+// remove nth node from tail end in one pass
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *curr = head;
+        ListNode *temp = head;
+
+        while (n--)
+        {
+            curr = curr->next;
+        }
+        if (!curr)
+            return head->next;
+
+        while (curr->next != NULL)
+        {
+            temp = temp->next;
+            curr = curr->next;
+        }
+
+        ListNode *bin = temp->next;
+        temp->next = temp->next->next;
+        delete (bin);
+        return head;
+    }
+};
