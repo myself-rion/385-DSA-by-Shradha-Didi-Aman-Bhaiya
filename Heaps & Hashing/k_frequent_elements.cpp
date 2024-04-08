abb676be3cccfb41adbf85b530be1e918e27c7b4
Rion Talukder <riontalukder98@gmail.com>
2024-04-08 22:00:51 +0530

    top K frequent elements in an array using max heap & min heap

--- Heaps & Hashing/k_frequent_elements.cpp
+++ Heaps & Hashing/k_frequent_elements.cpp
@@ -1,3 +1,4 @@
+// using max heap & comparator function
 class Solution
 {
 public:
@@ -25,3 +26,33 @@ public:
         return ans;
     }
 };
+
+// using min heap
+
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> m;
+        for (int i : nums)
+            m[i]++;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> p;
+        for (auto &it : m)
+        {
+            p.push({it.second, it.first});
+            if (p.size() > k)
+                p.pop();
+        }
+        vector<int> ans;
+        while (k--)
+        {
+            ans.push_back(p.top().second);
+            p.pop();
+        }
+        return ans;
+    }
+};
+
+// T.C: O(nlogk)
