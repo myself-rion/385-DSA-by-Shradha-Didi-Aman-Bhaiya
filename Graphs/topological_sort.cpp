98a17c7e0001bba73083ebce98dcad1dc7a97a15
Rion Talukder <riontalukder98@gmail.com>
2024-05-23 01:33:57 +0530

    Topological sort using DFS & Kahn's Algorithm

--- Graphs/topological_sort.cpp
+++ Graphs/topological_sort.cpp
@@ -1,38 +1,74 @@
-void topo(stack<int> &stck, vector<bool> &visited, int node, vector<int> adj[])
+class Solution
 {
-    visited[node] = true;
-
-    for (auto i : adj[node]) // accessing neighbouring elements
+public:
+    void dfs_topo(int i, bool vis[], vector<int> adj[], vector<int> &ans)
     {
+        vis[i] = true;
 
-        if (!visited[i])
+        for (auto nodes : adj[i])
+        {
+            if (!vis[nodes])
+            {
+                dfs_topo(nodes, vis, adj, ans);
+            }
+        }
 
-            topo(stck, visited, i, adj);
+        ans.push_back(i);
     }
-
-    stck.push(node);
-}
-// Function to return list containing vertices in Topological order.
-vector<int> topoSort(int V, vector<int> adj[])
-{
-    vector<int> ans;
-    vector<bool> visited(V, false);
-    stack<int> stck;
-
-    for (int i = 0; i < V; i++)
-    { // for disconnected components
-        if (!visited[i])
-            topo(stck, visited, i, adj); // recursive calls
-    }
-
-    while (!stck.empty())
+    vector<int> topoSort(int V, vector<int> adj[]) // using dfs
     {
-        ans.push_back(stck.top());
-        stck.pop();
+        bool vis[V] = {false};
+        //avoiding stack for extra auxiliary space & just reversing the ans array to get the LIFO results
+        vector<int> ans;
+
+        for (int i = 0; i < V; ++i)
+        {
+            if (!vis[i])
+            {
+                dfs_topo(i, vis, adj, ans);
+            }
+        }
+
+        reverse(begin(ans), end(ans));
+        return ans;
     }
 
-    return ans;
-}
 
-// T.C : O(V+E)
-// S.C : O(V)
+    //Kahn's Algo
+    vector<int> topoSort_using_bfs(int V, vector<int> adj[])
+    {
+        int inDegree[V] = {0}; //indegree of each vertex
+        vector<int> ans;
+
+        for (int i = 0; i < V; ++i)
+        {
+            for (auto &node : adj[i])
+            {
+                inDegree[node]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < V; ++i)
+        {
+            if (inDegree[i] == 0) //push vertices who have 0 indegree, means the traversal needs to start with them
+                q.push(i);
+        }
+
+        while (!q.empty())
+        {
+            int temp = q.front();
+            ans.push_back(temp);
+            q.pop();
+
+            for (auto &node : adj[temp])
+            {
+                inDegree[node]--;
+                if (inDegree[node] == 0) //push nodes who reached 0 indegree
+                    q.push(node);
+            }
+        }
+
+        return ans;
+    }
+};
